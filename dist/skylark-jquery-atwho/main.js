c93718d040901692c7bdee2fddfcb8c8f2ce0452
hudaokeji <github@hudaokeji.com>
2019-12-27 16:59:41 +0900

    init commit

---
+++ dist/skylark-jquery-atwho/main.js
@@ -0,0 +1,9 @@
+/**
+ * skylark-jquery-atwho - A version of jquery atwho that ported to running on skylarkjs
+ * @author Hudaokeji, Inc.
+ * @version v0.9.0
+ * @link https://github.com/skylark-integration/skylark-jquery-atwho/
+ * @license MIT
+ */
+define(["./atwho"],function(n){return n});
+//# sourceMappingURL=sourcemaps/main.js.map
